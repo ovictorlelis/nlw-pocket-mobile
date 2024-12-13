87b76fb5949755ae99964f7fd1b26f69342700ad
Victor Lelis <ovictorlelis@gmail.com>
2024-12-13 11:07:23 -0300

    Aula 01 finalizada

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,14 @@
 import { View, Text } from 'react-native'
 
+import { Welcome } from '@/components/welcome'
+import { Steps } from '@/components/steps'
+import { Step } from '@/components/step'
+
 export default function Index(){
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text style={{ fontSize: 22 }}>Hello React Native</Text>
+    <View style={{ flex: 1, padding: 40, gap: 40 }}>
+      <Welcome />
+      <Steps/>
     </View>
   )
 }
